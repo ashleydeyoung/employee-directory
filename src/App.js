8a6e571bc7a38a5ad19840e137b763aab93f8cac
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-30 19:54:48 -0400

    initial file structure and initial framework

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/Header";
+import DataTable from "./components/DataTable";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <DataTable />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Search from "./Search.js";
+
+function Header() {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-4">Employee Directory</h1>
+      <p className="lead">
+        Use the search box below to find employees by name
+      </p>
+      <Search />
+    </div>
+  );
+}
+
+export default Header;
